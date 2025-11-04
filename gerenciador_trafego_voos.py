79e2322e774ee6379e415a798f805fbd025bcc28
Vini <viniciusbiazuscastelan@gmail.com>
2025-11-04 18:44:02 -0300

    valor/bagagens

--- gerenciador_trafego_voos.py
+++ gerenciador_trafego_voos.py
@@ -2,6 +2,7 @@ from datetime import time, datetime, timedelta
 import uuid
 
 voos = {}
+passagens_compradas = {}
 log = []
 POSSIVEIS_STATUS = ['Cancelado', 'Agendado', 'Alterado', 'Atrasado']
 # CGH - SP, SDU - RJ, BSB - BRASILIA, CNF - BH, POA - PORTO
@@ -34,23 +35,35 @@ def fmt(td: timedelta) -> str:
     return f"{h:02d}:{m:02d}:{s:02d}"
 
 
-def adicionar_voo(codigo, horario, aeroporto_final, escalas=[]):
+def adicionar_voo(codigo, horario, limite_bagagem, ocupacao_total, valor_passagem, aeroporto_final, escalas=[], ):
     if codigo in voos:
         log.append('Esse voo já existe')
         return False
     ultima_escala = ORIGEM
     estimativa_de_tempo = timedelta()
     for escala in escalas:
-        rota = (ultima_escala + 'x' + escala)
-        estimativa_de_tempo += (to_timedelta(BLOCK_TIMES[rota]))
+        rota = [(ultima_escala + 'x' + escala),(escala + 'x' + ultima_escala)]
+        for possiveis_rotas in rota:
+            try:  
+                estimativa_de_tempo += (to_timedelta(BLOCK_TIMES[possiveis_rotas]))
+            except KeyError:
+                continue
         ultima_escala = escala
-    rota_final = (ultima_escala + 'x' + aeroporto_final)
-    estimativa_de_tempo += (to_timedelta(BLOCK_TIMES[rota_final]))
+    rota_final = [(ultima_escala + 'x' + aeroporto_final), (aeroporto_final + 'x' + ultima_escala)]
+    for possiveis_rotas_finais in rota_final:
+        try:
+            estimativa_de_tempo += (to_timedelta(BLOCK_TIMES[possiveis_rotas_finais]))
+        except KeyError:
+            continue
     voos[codigo] = {
         'destino': aeroporto_final,
         'horario': horario,
         'status': 'Agendado',
-        'tempo aproximado': fmt(estimativa_de_tempo)
+        'tempo aproximado': fmt(estimativa_de_tempo),
+        'limite de bagagem': limite_bagagem,
+        'ocupação total': ocupacao_total,
+        'quantidade atual de passageiros': 0,
+        'valor da passagem': valor_passagem
     }
     log.append(f'Voo {codigo} adicionado com sucesso')
 
@@ -90,34 +103,40 @@ def consultar_voo(codigo):
         print(voos[codigo])
         log.append(f'Consultou o voo {codigo}')
 
-
-passagens_compradas = {}
-def comprar_passagem(codigo, nome_do_passageiro,):
+def comprar_passagem(codigo, nome_do_passageiro, numero_de_bagagens):
+    if codigo not in voos:
+        log.append('Esse código não existe para ser comprado')
+    if 0 < numero_de_bagagens > 5:
+        log.append(f'A quantidade de bagagem miníma é 0 e o máximo 5, atualmente {numero_de_bagagens}')
+    voos[codigo]['limite de bagagem'] -= numero_de_bagagens
     id_compra = uuid.uuid4().hex
     passagens_compradas[id_compra] = {
         'Codigo do voo': codigo,
         'Nome': nome_do_passageiro,
         'Status do pagamento': 'Pendente',
+        'Bagagens': numero_de_bagagens
     }
-    log.append(f'O passageiro {nome_do_passageiro} comprou uma passagem para o voo {codigo}, código de compra {id_compra}')
+    log.append(f'O passageiro {nome_do_passageiro} comprou uma passagem para o voo {codigo}, levando {numero_de_bagagens}, código de compra {id_compra}')
     return id_compra
 
-passagem = comprar_passagem('VB999', 'VINICIUS')
 
 def pagar_passagem(id_compra):
     passagens_compradas[id_compra]['Status do pagamento'] = 'Pago'
+    codigo_de_voo = passagens_compradas[id_compra]['Codigo do voo']
+    voos[codigo_de_voo]['quantidade atual de passageiros'] += 1
     log.append(f'A passagem {passagens_compradas[id_compra]} foi paga.')
 
-
-adicionar_voo('VB999', '06:00', 'BSB')
-adicionar_voo('AB123', '01:00', 'CNF', ['CGH'])
-consultar_voo('VB999')
-alterar_status('VB999', 'Atrasado')
-print(listar_voos())
-cancelar_voo('VB999')
-consultar_voo('VB99')
-pagar_passagem(passagem)
-
+def testar_codigo():
+    adicionar_voo('VB999', '06:00', 100, 300, 500.00, 'BSB')
+    passagem = comprar_passagem('VB999', 'VINICIUS', 2)
+    adicionar_voo('TNC000', '23:00', 100, 300, 500.00, 'BSB', ['SDU', 'CGH'])
+    consultar_voo('VB999')
+    alterar_status('VB999', 'Atrasado')
+    cancelar_voo('TNC000')
+    consultar_voo('VB99')
+    pagar_passagem(passagem)
+    print(listar_voos())
+testar_codigo()
 
 with open('log.txt', 'w', encoding='utf-8') as f:
     for ocorrencia in log:
